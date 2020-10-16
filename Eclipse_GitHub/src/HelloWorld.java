c8a2f797686df85eb7c44e0c1cc56c673920b1a4
BoGyeong1103 <61460840+BoGyeong1103@users.noreply.github.com>
2020-10-16 18:35:07 +0900

    Update HelloWorld.java

--- Eclipse_GitHub/src/HelloWorld.java
+++ Eclipse_GitHub/src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world !!");
+		System.out.println("This is master brunch !!");
+		System.out.println("Hi Hello");
 		System.out.println("Conflict Occurred !!");
 	}
 }
